7887d66b7ef4042b0e986edad4a5ef546a1a080d
jean <jeancarlospenas25@gmail.com>
2018-06-21 23:01:58 -0300

    teste

--- config/app.php
+++ config/app.php
@@ -217,6 +217,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Settings' => Oriceon\Settings\Facades\Settings::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
