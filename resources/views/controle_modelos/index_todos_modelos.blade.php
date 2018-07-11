3c6c572e8180dd5c6a58e58cb70b7a3ad6350b70
Unknown <jeancarlospenas25@gmail.com>
2018-07-11 01:48:18 -0300

    Alteração template - visualização de todos os modelos

--- resources/views/controle_modelos/index_todos_modelos.blade.php
+++ resources/views/controle_modelos/index_todos_modelos.blade.php
@@ -1,22 +1,40 @@
-@extends('layouts.modelagem.main_area_modelador2')
+{{--@extends('layouts.modelagem.main_area_modelador2')--}}
 
 
-@section('content')
-    {!! csrf_field() !!}
+{{--@section('content')--}}
+    {{--{!! csrf_field() !!}--}}
 
-    <h4>Modelos Cadastrados No Sistema</h4>
+    {{--<h4>Modelos Cadastrados No Sistema</h4>--}}
 
-    <br>
+    {{--<br>--}}
     {{--$dados--}}
     {{--$titulos--}}
     {{--$rota_cricao--}}
 
-    @include('componentes.tabela_todos',
-                                [
-                                'titulos' => $titulos,
-                                'dados' => $modelos,
-                                'rota_exibicao' => 'controle_modelos.show',
-                                'nomebotao' => 'Visualizar Modelo',
-                                'rota_exclusao' => 'controle_modelos.destroy'
-                                ])
+    {{--@include('componentes.tabela_todos',--}}
+                                {{--[--}}
+                                {{--'titulos' => $titulos,--}}
+                                {{--'dados' => $modelos,--}}
+                                {{--'rota_exibicao' => 'controle_modelos.show',--}}
+                                {{--'nomebotao' => 'Visualizar Modelo',--}}
+                                {{--'rota_exclusao' => 'controle_modelos.destroy'--}}
+                                {{--])--}}
+{{--@endsection--}}
+
+
+
+@extends('layouts.layout_admin_new.layouts.main')
+
+@section('content')
+    {!! csrf_field() !!}
+    @includeIf('layouts.layout_admin_new.componentes.breadcrumb',['titulo' => 'Todos os modelos'])
+
+    @includeIf('layouts.layout_admin_new.componentes.tables',[
+                    'titulos' => $titulos,
+                    'modelos' => $modelos,
+                    'rota_exclusao' => 'controle_modelos.destroy',
+                    'rota_exibicao' => 'controle_modelos.show',
+                    'nome_botao' => 'Novo',
+                    'titulo' =>'Modelos'
+    ])
 @endsection
