66bbce4a933a638a4d465297a3724a41ff4aa4ae
Unknown <jeancarlospenas25@gmail.com>
2018-07-12 10:37:08 -0300

    Correção  - Layout da página do modelo diagramático

--- resources/views/controle_modelos/form_diagramatico.blade.php
+++ resources/views/controle_modelos/form_diagramatico.blade.php
@@ -1,6 +1,16 @@
-@extends('layouts.modelagem.main_area_modelador2')
+{{--@extends('layouts.modelagem.main_area_modelador2')--}}
+
+{{--@section('content')--}}
+    {{--<h4>Modelo Diagramático</h4>--}}
+
+{{--@endsection--}}
+
+@extends('layouts.layout_admin_new.layouts.main')
 
 @section('content')
-    <h4>Modelo Diagramático</h4>
+    {!! csrf_field() !!}
+    @includeIf('layouts.layout_admin_new.componentes.breadcrumb',[
+                    'titulo' => 'Modelo Disgramático'
+    ])
 
 @endsection
