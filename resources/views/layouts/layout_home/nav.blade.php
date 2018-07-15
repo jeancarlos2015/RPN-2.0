abd1ec4fd5d0b1bd5fcee24c9c1b5dedbcec470d
Unknown <jeancarlospenas25@gmail.com>
2018-07-15 19:19:28 -0300

    Menu - Alterção

--- resources/views/layouts/layout_home/nav.blade.php
+++ resources/views/layouts/layout_home/nav.blade.php
@@ -1,7 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="{{ route('/') }}">RPN - Repositório de Processos de Negócio</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
         </button>
@@ -10,19 +12,13 @@
                 <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="{{ route('/') }}">Início</a>
                 </li>
-
                 <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="{{ route('painel') }}">Sistema</a>
+                    <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">{{ trans('auth.Login') }}</a>
                 </li>
-
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">{{ trans('auth.Login') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="{{ route('register') }}">{{ trans('auth.Register') }}</a>
-                    </li>
-                @endguest
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger"
+                       href="{{ route('register') }}">{{ trans('auth.Register') }}</a>
+                </li>
             </ul>
         </div>
     </div>
