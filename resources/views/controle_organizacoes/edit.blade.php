558630db71669ef3e195d14620cf626c0daf3563
Unknown <jeancarlospenas25@gmail.com>
2018-07-15 15:54:20 -0300

    Correção - Edição de organização

--- resources/views/controle_organizacoes/edit.blade.php
+++ resources/views/controle_organizacoes/edit.blade.php
@@ -1,10 +1,28 @@
-@extends('layouts.modelagem.main_area_modelador2')
+{{--@extends('layouts.modelagem.main_area_modelador2')--}}
 
+
+{{--@section('content')--}}
+    {{--<h3>Editar o Projeto</h3>--}}
+    {{--<form action="{!! route('controle_organizacoes.update',[$organizacao->codorganizacao]) !!}" method="POST">--}}
+        {{--{{ method_field('PUT')}}--}}
+        {{--@includeIf('controle_organizacoes.form',['acao' => 'Atualizar e Proseguir','dados' => $dados,'MAX' => 2])--}}
+    {{--</form>--}}
+{{--@endsection--}}
+
+
+@extends('layouts.layout_admin_new.layouts.main')
 
 @section('content')
-    <h3>Editar o Projeto</h3>
+    {!! csrf_field() !!}
+    @includeIf('layouts.layout_admin_new.componentes.breadcrumb',[
+                    'titulo' => 'Paianel',
+                    'sub_titulo' => 'Organizacoes',
+                    'rota' => 'controle_organizacoes.index'
+    ])
+
     <form action="{!! route('controle_organizacoes.update',[$organizacao->codorganizacao]) !!}" method="POST">
-        {{ method_field('PUT')}}
-        @includeIf('controle_organizacoes.form',['acao' => 'Atualizar e Proseguir','dados' => $dados,'MAX' => 2])
+    {{ method_field('PUT')}}
+    @includeIf('controle_organizacoes.form',['acao' => 'Atualizar e Proseguir','dados' => $dados,'MAX' => 2])
     </form>
+
 @endsection
