0af849b0e45f14d4c4967e377bcb30515d561418
Unknown <jeancarlospenas25@gmail.com>
2018-07-16 01:19:53 -0300

    Teste

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -144,12 +144,13 @@ class GitController extends Controller
 
     private function checkout(Request $request)
     {
-        $git = new GitSistemaRepository();
-//        if ($git->is_exchanges()) {
-//            $git->git_commit('checkout');
-//        }
+
 //        $git->git_checkout_branch($request->branch);
         try {
+            $git = new GitSistemaRepository();
+            if ($git->is_exchanges()) {
+                $git->git_commit('checkout');
+            }
             $git_comando = new GitComando($git->get_path());
             $git_comando->checkout($request->branch);
         } catch (GitException $e) {
