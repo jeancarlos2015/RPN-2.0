1d223f20cc347c78566cd20249ca777d9b9ceb30
Unknown <jeancarlospenas25@gmail.com>
2018-07-20 20:43:23 -0300

    Correção - Redefinição de senha

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://projetolaravel21.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
