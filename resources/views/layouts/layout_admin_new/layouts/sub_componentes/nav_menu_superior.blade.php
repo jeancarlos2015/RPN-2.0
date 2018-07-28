b1d64e743ea8c0c9e7c99e64eb5953fc89d1aa45
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-07-28 15:55:38 -0300

    Correção - atualizar repositório

--- resources/views/layouts/layout_admin_new/layouts/sub_componentes/nav_menu_superior.blade.php
+++ resources/views/layouts/layout_admin_new/layouts/sub_componentes/nav_menu_superior.blade.php
@@ -5,13 +5,13 @@
             Opções
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item btn-secondary" href="{!! route('painel') !!}">Painel</a>
-            <a class="dropdown-item btn-secondary" href="{!! route('index_logs') !!}">Logs do Sistema</a>
+            <a class="dropdown-item" href="{!! route('painel') !!}">Painel</a>
+            <a class="dropdown-item" href="{!! route('index_logs') !!}">Logs do Sistema</a>
             @if(!empty(Auth::user()->github->branch_atual) && !empty(Auth::user()->github->repositorio_atual))
-                <a class="dropdown-item btn-secondary" href="{!! route('pull') !!}">Atualizar</a>
-                <a class="dropdown-item btn-secondary" href="{!! route('index_init') !!}">Repositórios</a>
+                <a class="dropdown-item" href="{!! route('pull') !!}">Atualizar</a>
+                <a class="dropdown-item" href="{!! route('index_init') !!}">Repositórios</a>
             @endif
-            <a class="dropdown-item btn-secondary" href="{!! route('controle_documentacoes.index') !!}">Documentação do
+            <a class="dropdown-item" href="{!! route('controle_documentacoes.index') !!}">Documentação do
                 Sistema</a>
 
         </div>
--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::prefix('admin')->middleware(['auth'])->group(
         Route::post('init', 'GitController@init')->name('init');
         Route::post('create', 'GitController@create')->name('create');
         Route::post('commit', 'GitController@commit')->name('commit');
-        Route::post('pull', 'GitController@pull')->name('pull');
+        Route::get('pull', 'GitController@pull')->name('pull');
 
         Route::post('merge_checkout', 'GitController@merge_checkout')->name('merge_checkout');
         Route::post('delete', 'GitController@delete')->name('delete');
