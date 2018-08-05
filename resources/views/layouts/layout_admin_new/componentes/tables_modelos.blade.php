2200dffb1b8cd9642c33bbe7b4ea2f46cbfede0f
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-08-05 13:27:29 -0300

    Edição de modelos diagramáticos

--- resources/views/layouts/layout_admin_new/componentes/tables_modelos.blade.php
+++ resources/views/layouts/layout_admin_new/componentes/tables_modelos.blade.php
@@ -9,7 +9,13 @@
             <td>
 
                 @if(!empty($rota_edicao))
-                    @include('componentes.link',['id' => $modelo1->codmodelo, 'rota' => $rota_edicao])
+                    @if($modelo1->tipo==='diagramatico')
+
+                        @include('componentes.link',['id' => $modelo1->codmodelo, 'rota' => 'criacao_modelo_diagramatico'])
+                    @else
+                        @include('componentes.link',['id' => $modelo1->codmodelo, 'rota' => $rota_edicao])
+                    @endif
+
                 @endif
                 @if(!empty($rota_exclusao))
                     @include('componentes.form_delete',['id' => $modelo1->codmodelo, 'rota' => $rota_exclusao])
