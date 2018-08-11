1aadf84ccaefaf0fd422359ecfac7ab9c65df384
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-08-11 14:41:00 -0300

    Correção - Edição de repositórios

--- resources/views/controle_repositorios/edit.blade.php
+++ resources/views/controle_repositorios/edit.blade.php
@@ -8,7 +8,7 @@
                     'rota' => 'controle_repositorios.index'
     ])
 
-    <form action="{!! route('controle_repositorios',[$repositorio->codrepositorio]) !!}" method="POST">
+    <form action="{!! route('controle_repositorios.update',[$repositorio->codrepositorio]) !!}" method="POST">
     {{ method_field('PUT')}}
     @includeIf('controle_repositorios.form',['acao' => 'Atualizar e Proseguir','dados' => $dados,'MAX' => 2])
     </form>
