ce54f4a3ec9e329f57be00c22b6acb0ec652c72b
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-08-11 00:41:54 -0300

    teste

--- app/Http/Controllers/RepositorioController.php
+++ app/Http/Controllers/RepositorioController.php
@@ -25,6 +25,7 @@ class RepositorioController extends Controller
     {
         try {
             $repositorios = RepositorioRepository::listar();
+//            dd($repositorios[0]->modelos->count());
             $titulos = Repositorio::titulos_da_tabela();
             $campos = Repositorio::campos();
             $tipo = 'repositorio';
--- resources/views/layouts/layout_admin_new/componentes/tables_repositorios.blade.php
+++ resources/views/layouts/layout_admin_new/componentes/tables_repositorios.blade.php
@@ -13,9 +13,9 @@
                         <div class="media-body">
                             <strong>{!!  $repositorio1->nome !!}</strong>
                             <div class="text-muted smaller">Repositório: {!! $repositorio1->nome !!}</div>
-                            {{--<div class="text-muted smaller">Usuários: {!! count($repositorio1->usuarios) !!}</div>--}}
-                            {{--<div class="text-muted smaller">Projetos: {!! count($repositorio1->projetos) !!}</div>--}}
-                            <div class="text-muted smaller">Modelos: {!! count($repositorio1->modelos) !!}</div>
+                            <div class="text-muted smaller">Usuários: {!! $repositorio1->usuarios->count() !!}</div>
+                            <div class="text-muted smaller">Projetos: {!! $repositorio1->projetos->count() !!}</div>
+                            <div class="text-muted smaller">Modelos: {!! $repositorio1->modelos->count() !!}</div>
                         </div>
                     </div>
                 </a>
