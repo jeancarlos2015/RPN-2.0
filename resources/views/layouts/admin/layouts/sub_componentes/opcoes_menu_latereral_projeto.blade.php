492cf8dea2d67480c72f532d44770987ca4b0964
Unknown <jeancarlospenas25@gmail.com>
2018-08-17 13:51:30 -0300

    Implementacao inicial das regras

--- resources/views/layouts/admin/layouts/sub_componentes/opcoes_menu_latereral_projeto.blade.php
+++ resources/views/layouts/admin/layouts/sub_componentes/opcoes_menu_latereral_projeto.blade.php
@@ -3,6 +3,8 @@
         <i class="fa fa-fw fa-list"></i>
         <span class="nav-link-text">Sistema</span>
     </a>
+
+
     <ul class="sidenav-second-level collapse" id="collapseComponents1">
 
         <li>
@@ -10,6 +12,10 @@
         </li>
         <li>
             <a href="{!! route('todos_modelos') !!}"><i class="fa fa-fw fa-pencil"></i>Modelos</a>
+        </li>
+        
+        <li>
+            <a href="{!! route('controle_regras.index') !!}"><i class="fa fa-fw fa-pencil"></i>Regras</a>
         </li>
         <li>
             <a href="{!! route('controle_objetos_fluxos.index') !!}"><i class="fa fa-fw fa-pencil"></i>Objetos De Fluxo</a>
