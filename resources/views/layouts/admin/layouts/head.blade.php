ba10dc259755a31ab7ec1eb531d99f3148137b67
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-08-17 00:01:50 -0300

    Refatoracao view

--- resources/views/layouts/admin/layouts/head.blade.php
+++ resources/views/layouts/admin/layouts/head.blade.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,5 +19,4 @@
     <!-- Custom styles for this template_basico-->
     <link href="{!! asset('css/sb-admin.css') !!}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/css-toggle-switch/latest/toggle-switch.css"/>
-
 </head>
