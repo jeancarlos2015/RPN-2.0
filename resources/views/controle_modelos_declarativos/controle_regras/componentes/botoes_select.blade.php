30615b32c61427354500c6cf20404b1403accc09
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-08-18 23:09:13 -0300

    Refatoracao

--- resources/views/controle_modelos_declarativos/controle_regras/componentes/botoes_select.blade.php
+++ resources/views/controle_modelos_declarativos/controle_regras/componentes/botoes_select.blade.php
@@ -1,25 +1,12 @@
-{{--<div class="col-12">--}}
-    {{--<div class="subject-info-arrows text-center">--}}
-        <div class="col-3">
-            <input type='button' id='rightall' value='>>' class="btn btn-default"/><br/>
-        </div>
-        <div class="col-3">
-            <input type='button' id='right' value='>' class="btn btn-default"/>
-        </div>
-        <div class="col-3">
-            <input type='button' id='left' value='<' class="btn btn-default"/>
-        </div>
-        <div class="col-3">
-            <input type='button' id='leftall' value='<<' class="btn btn-default"/>
-        </div>
-
-
-
-
-
-
-
-
-
-    {{--</div>--}}
-{{--</div>--}}
+<div class="col-3">
+    <input type='button' id='rightall' value='>>' class="btn btn-default"/><br/>
+</div>
+<div class="col-3">
+    <input type='button' id='right' value='>' class="btn btn-default"/>
+</div>
+<div class="col-3">
+    <input type='button' id='left' value='<' class="btn btn-default"/>
+</div>
+<div class="col-3">
+    <input type='button' id='leftall' value='<<' class="btn btn-default"/>
+</div>
