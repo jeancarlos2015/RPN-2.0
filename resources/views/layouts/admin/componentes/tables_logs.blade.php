cc505b103fb1e47da80960e06b927117ad5d1d28
Unknown <jeancarlospenas25@gmail.com>
2018-08-20 14:06:27 -0300

    correção listagem de logs

--- resources/views/layouts/admin/componentes/tables_logs.blade.php
+++ resources/views/layouts/admin/componentes/tables_logs.blade.php
@@ -3,13 +3,32 @@
 
     @foreach($logs as $log)
         <tr>
-            <td>{!! $log->codlog !!}</td>
-            <td>{!! $log->nome !!}</td>
-            <td>{!! $log->descricao !!}</td>
+            {{--<td>{!! $log->codlog !!}</td>--}}
+            {{--<td>{!! $log->nome !!}</td>--}}
+            {{--<td>{!! $log->descricao !!}</td>--}}
             {{--<td>{!! $log->usuario->name !!}</td>--}}
-            <td>{!! $log->created_at !!}</td>
-            <td>{!! $log->pagina !!}</td>
-            <td>{!! $log->acao !!}</td>
+            {{--<td>{!! $log->created_at !!}</td>--}}
+            {{--<td>{!! $log->pagina !!}</td>--}}
+            {{--<td>{!! $log->acao !!}</td>--}}
+            <td>
+                {{--<a href="{!! route($rota_exibicao,[$repositorio1->codrepositorio]) !!}">--}}
+                <div class="media">
+                    <img class="d-flex mr-3 rounded-circle" src="{{ Gravatar::src('public/img/processo.png') }}"
+                         alt="" width="100">
+                    <div class="media-body">
+                        <strong>{!!  $log->nome !!}</strong>
+                        <div class="text-muted smaller">Nome: {!! $log->nome !!}</div>
+                        <div class="text-muted smaller">Descrição: {!! count($log->usuarios) !!}</div>
+                        @if(!empty($log->usuario->name))
+                            <div class="text-muted smaller">Usuário: {!! $log->usuario->name !!}</div>
+                        @endif
+                        <div class="text-muted smaller">Data: {!! $log->created_at !!}</div>
+                        <div class="text-muted smaller">Página: {!! $log->pagina !!}</div>
+                        <div class="text-muted smaller">Método que disparou o erro: {!! $log->acao !!}</div>
+                    </div>
+                </div>
+                {{--</a>--}}
+            </td>
             <td>
                 @if (Auth::user()->email === 'jeancarlospenas25@gmail.com' || Auth::user()->tipo==='Administrador')
                     @if(!empty($rota_edicao))
