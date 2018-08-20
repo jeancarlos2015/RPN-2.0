3c4a5916d648b4a24ecc495f9308694eff3b6c90
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-08-20 01:59:49 -0300

    atualizacao projeto

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.33 on 2018-08-19 23:02:21.
+ * Generated for Laravel 5.6.33 on 2018-08-20 04:59:17.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
