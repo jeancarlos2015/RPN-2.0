cea250c69bed0088c97b2a0aec838c5c40e472d1
Unknown <jeancarlospenas25@gmail.com>
2018-08-21 14:00:32 -0300

    Correção - Opções do menu superior não aparecem ao criar o modelo diagramático

--- resources/views/controle_modelos_diagramaticos/layout_diagrama/nav.blade.php
+++ resources/views/controle_modelos_diagramaticos/layout_diagrama/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    @includeIf('layouts.admin.layouts.sub_componentes.nav_menu_titulo')
+    @includeIf('menu.nav_menu_titulo')
     <div class="collapse navbar-collapse" id="navbarResponsive">
-        @includeIf('layouts.admin.layouts.sub_componentes.nav_menu_superior')
+        @includeIf('menu.nav_menu_superior')
     </div>
 </nav>
