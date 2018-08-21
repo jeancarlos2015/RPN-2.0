1013f3e1e67c977799273ff54cc958e7465ce26f
Unknown <jeancarlospenas25@gmail.com>
2018-08-21 13:42:20 -0300

    Correção de visualização de repositórios pelo administrador do sistema

--- app/Http/Repositorys/RepositorioRepository.php
+++ app/Http/Repositorys/RepositorioRepository.php
@@ -18,7 +18,7 @@ class RepositorioRepository extends Repository
 
     public static function listar()
     {
-        if (Auth::user()->email === 'jeancarlospenas25@gmail.com' || Auth::user()->type==='Administrador') {
+        if (Auth::user()->email === 'jeancarlospenas25@gmail.com' || Auth::user()->tipo==='Administrador') {
             return collect(Repositorio::all());
         }
         return collect(array());
