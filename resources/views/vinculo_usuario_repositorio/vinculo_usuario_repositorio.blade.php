8b9d4f8d586884086a03572937706fe8fc2270d6
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2018-08-25 20:13:32 -0300

    correção de layout controle de usuários

--- resources/views/vinculo_usuario_repositorio/vinculo_usuario_repositorio.blade.php
+++ resources/views/vinculo_usuario_repositorio/vinculo_usuario_repositorio.blade.php
@@ -1,23 +1,49 @@
+{{--@extends('layouts.admin.layouts.main')--}}
+{{--@section('content')--}}
+{{--@includeIf('layouts.admin.componentes.breadcrumb',[--}}
+{{--'titulo' => 'Painel',--}}
+{{--'sub_titulo' => 'Usuários',--}}
+{{--'rota' => 'painel'--}}
+{{--])--}}
+{{--@includeIf('layouts.admin.componentes.tables',[--}}
+{{--'titulos' => $titulos,--}}
+{{--'usuarios' => $usuarios,--}}
+{{--'rota_edicao' => 'controle_usuarios.edit',--}}
+{{--'rota_exclusao' => 'controle_usuarios.destroy',--}}
+{{--'nome_botao' => 'Novo',--}}
+{{--'titulo' =>'Usuarios'--}}
+{{--])--}}
+{{--@endsection--}}
+{{--@section('modo')--}}
+{{--@includeIf('componentes.descricao',[--}}
+{{--'descricao_titulo_menu' => 'Modo de Vinculação e Desvinculação',--}}
+{{--'nome_titulo_menu' => 'Modo de Vinculação e Desvinculação'--}}
+{{--])--}}
+{{--@endsection--}}
+
 
 @extends('layouts.admin.layouts.main')
 @section('content')
     @includeIf('layouts.admin.componentes.breadcrumb',[
-                      'titulo' => 'Painel',
+                    'titulo' => 'Painel',
                     'sub_titulo' => 'Usuários',
                     'rota' => 'painel'
     ])
+    @includeIf('layouts.admin.componentes.botao',['tipo' => $tipo])
     @includeIf('layouts.admin.componentes.tables',[
-                       'titulos' => $titulos,
-                       'usuarios' => $usuarios,
-                       'rota_edicao' => 'controle_usuarios.edit',
-                       'rota_exclusao' => 'controle_usuarios.destroy',
-                       'nome_botao' => 'Novo',
-                       'titulo' =>'Usuarios'
-       ])
+                    'titulos' => $titulos,
+                    'repositorios' => $usuarios,
+                    'rota_edicao' => 'controle_usuarios.edit',
+                    'rota_exclusao' => 'controle_usuarios.destroy',
+                    'nome_botao' => 'Novo',
+                    'titulo' =>'Repositórios - Clique no Usuário desejado para editar suas credenciais e vínculos!!',
+                    'tipo' => $tipo
+
+    ])
 @endsection
 @section('modo')
     @includeIf('componentes.descricao',[
-        'descricao_titulo_menu' => 'Modo de Vinculação e Desvinculação',
-        'nome_titulo_menu' => 'Modo de Vinculação e Desvinculação'
+        'descricao_titulo_menu' => 'Controle de Usuários',
+        'nome_titulo_menu' => 'Usuários'
     ])
 @endsection
